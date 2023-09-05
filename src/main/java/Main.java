b007e4e0018f2e67b3c466c5bbded455db0bae76
Duc Anh <anhduong764027@gmail.com>
2023-09-05 11:54:23 +0700

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+
+import Common.Library;
+import Controller.SortProgramming;
+import Model.Element;
+
+
+public class Main {
+    public static void main(String[] args) {
+       
+        Element element = new Element();
+       
+  
+        new SortProgramming(element).run();
+    }
+}
